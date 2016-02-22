5c0ea178612a832bb742d89d6bf18aeab8844222
Kyle Hahnel <empire5790@gmail.com>
2016-02-22 16:36:26 +0000

    initial commit

---
+++ lumberjack.py
@@ -0,0 +1,6 @@
+name = input("What's your name? ")
+
+if name == "Kyle":
+  print(" {} is a lumberjack and he's OK! ".format(name))
+else:
+  print(" {} sleeps all night and {} works all day! ".format(name, name))   
